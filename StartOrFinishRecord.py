54836ddbda56869a9ca320519eff2f291dde8faf
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2024-08-21 09:35:45 +0900

    スクリプト修正

--- StartOrFinishRecord.py
+++ StartOrFinishRecord.py
@@ -2,10 +2,12 @@ import pyautogui
 import time
 
 try:
+    print('録画を実行します。')
     # 2秒待機してからキー操作を開始
     time.sleep(2)
 
     # Windows + Alt + R を押す
     pyautogui.hotkey('win', 'alt', 'r')
+    print('キー操作を終了しました。')
 except Exception as e:
     print(e)

e002e8748b5f1f66e025f0db4e66c423cdf2e2e6
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2024-08-21 09:56:49 +0900

    実行前マージン追加

--- StartOrFinishRecord.py
+++ StartOrFinishRecord.py
@@ -3,11 +3,15 @@ import time
 
 try:
     print('録画を実行します。')
+    
     # 2秒待機してからキー操作を開始
-    time.sleep(2)
+    time.sleep(5)
 
     # Windows + Alt + R を押す
     pyautogui.hotkey('win', 'alt', 'r')
-    print('キー操作を終了しました。')
+    
+    print('キー操作を実行しました。')
+    
+    time.sleep(2)
 except Exception as e:
     print(e)

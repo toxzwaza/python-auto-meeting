c4d87aa79363bcd1a15bfa74e929b9bbbb869f20
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2024-08-26 18:46:18 +0900

    finish alt+f4 -> psutil

--- finishRecord.py
+++ finishRecord.py
@@ -1,13 +1,21 @@
 import keyboard
 import time
+import psutil
+
+def kill_process_by_name_part(name_part):
+    for proc in psutil.process_iter(['pid', 'name']):
+        if name_part.lower() in proc.info['name'].lower():
+            proc.kill()
+            print(f"Process {proc.info['name']} (PID: {proc.info['pid']}) has been terminated.")
+            
 
 try:
     print('録画開始・終了を実行します。')
     # 2秒待機してからキー操作を開始
     time.sleep(2)
 
-    # Windows + Alt + R を押す
-    keyboard.press_and_release('alt+F4')
+    # 例: タスク名に 'Teams' が含まれているプロセスを閉じる
+    kill_process_by_name_part('Teams')
     
     
     # 2秒待機してからキー操作を開始

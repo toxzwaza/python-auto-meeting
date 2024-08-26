e073c54bcddc5cc7ca999c8095e47f3a513a035d
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2024-08-26 18:26:24 +0900

    startScriptとfinishScript

---
+++ finishRecord.py
@@ -0,0 +1,23 @@
+import keyboard
+import time
+
+try:
+    print('録画開始・終了を実行します。')
+    # 2秒待機してからキー操作を開始
+    time.sleep(2)
+
+    # Windows + Alt + R を押す
+    keyboard.press_and_release('alt+F4')
+    
+    
+    # 2秒待機してからキー操作を開始
+    time.sleep(5)
+
+    # Windows + Alt + R を押す
+    keyboard.press_and_release('ctrl+shift+F12')
+    
+    print('キー操作を実行しました。')
+    
+    time.sleep(2)
+except Exception as e:
+    print(e)
